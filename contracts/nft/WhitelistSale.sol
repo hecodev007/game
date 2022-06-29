bdc07bfa2e1a74cf88621fc4e76abb742835b662
tanyz <920993517@qq.com>
2022-06-29 14:10:32 +0800

    bug

--- contracts/nft/WhitelistSale.sol
+++ contracts/nft/WhitelistSale.sol
@@ -6,15 +6,15 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract WhitelistSale is ERC721 {
     bytes32 public merkleRoot;
     uint256 public nextTokenId;
-    mapping(address => bool) public claimed;
+    mapping(uint256 => bool) public claimed;
 
     constructor(bytes32 _merkleRoot) public ERC721("NFT", "NFT") {
         merkleRoot = _merkleRoot;
     }
 
-    function mint(bytes32[] calldata merkleProof) public payable {
-        require(claimed[msg.sender] == false, "already claimed");
-        claimed[msg.sender] = true;
+    function mint(bytes32[] calldata merkleProof,uint256 id) public payable {
+        require(claimed[id] == false, "already claimed");
+        claimed[id] = true;
         require(MerkleProof.verify(merkleProof, merkleRoot, keccak256(abi.encodePacked(msg.sender))), "invalid merkle proof");
         nextTokenId++;
         _mint(msg.sender, nextTokenId);
